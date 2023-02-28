a02829eacf8228bad521fd08bac55a3e521861ba
João Capucho <jcapucho7@gmail.com>
2023-02-28 15:50:40 +0000

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,25 +1,43 @@
 {
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nci.url = "github:yusdacra/nix-cargo-integration";
+    nci.inputs.nixpkgs.follows = "nixpkgs";
+    parts.url = "github:hercules-ci/flake-parts";
+    parts.inputs.nixpkgs-lib.follows = "nixpkgs";
   };
-  outputs = inputs:
-    inputs.nci.lib.makeOutputs {
-      root = ./.;
-      config = common: {
-        shell = {
-          packages = with common.pkgs; [cargo-deny pandoc rust-analyzer tree-sitter];
-        };
-        outputs.defaults = {
-          app = "pandoc-norg-rs";
-          package = "pandoc-norg-rs";
-        };
-      };
-      pkgConfig = common: {
-        pandoc-norg-rs.overrides = {
-          add-test-inputs = {
-            buildInputs = [common.pkgs.pandoc];
+  outputs = inputs @ {
+    parts,
+    nci,
+    ...
+  }:
+    parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      imports = [nci.flakeModule];
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: let
+        crateName = "pandoc-norg-rs";
+        crateOutputs = config.nci.outputs.${crateName};
+      in {
+        nci.projects.${crateName}.relPath = "";
+        # configure crates
+        nci.crates.${crateName} = {
+          export = true;
+          overrides = {
+            add-tests-inputs.overrideAttrs = old: {
+              buildInputs = (old.buildInputs or []) ++ [pkgs.pandoc];
+            };
           };
         };
+        devShells.default = crateOutputs.devShell.overrideAttrs (old: let
+          extraPackages = with pkgs; [cargo-deny pandoc rust-analyzer tree-sitter];
+        in {
+          packages = (old.packages or []) ++ extraPackages;
+        });
+        packages.default = crateOutputs.packages.release;
       };
     };
 }
